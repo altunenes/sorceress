9fcfb8476a2010a135beebcd1c7d32076d5c3c1a
emportent <enesaltun2@gmail.com>
2021-09-08 12:22:06 +0300

    test test

---
+++ sorceress/test___init__.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+
+
+class Testsorcerer(TestCase):
+    def test_chromatic(self):
+        return
+
+    def test_dotill(self):
+        return
+    def test_realtimegrid(self):
+        return
+    def test_addlines(self):
+        return
+    def test_addlines_alpha(self):
+        return
+    def test_eyecolour(self):
+        return
+    def test_dakin_pex(self):
+        return
+    def test_bruno(self):
+        return
+    def test_dolboeuf(self):
+        return
+    def test_kanizsa(self):
+        return
+    def test_ponzol(self):
+        return
