e387b6f7a40f383800cb8cf96f5ed8608c2df5c0
Enes <54986652+emportent@users.noreply.github.com>
2021-09-08 12:36:05 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ classifiers = [
 
 setup(
     name='sorceress',
-    version='1.4.1',
+    version='1.4.2',
     description='Optical Illusions with Python',
     long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
     url='https://github.com/emportent/sorceress',
