8effea3c067d580a04bb81b254d80279f1a4ee9e
altunenes <enesaltun2@gmail.com>
2022-12-16 13:00:51 +0300

    new function: colordetection

--- sorceress/sorceress/sorceress.py
+++ sorceress/sorceress/sorceress.py
@@ -1162,3 +1162,31 @@ def negate_image(input_file, negation_method, color_space, output_quality):
     output_file = filename + '_neg.png'
 
     cv2.imwrite(output_file, neg_img, [cv2.IMWRITE_PNG_COMPRESSION, output_quality])
+def colordetection(inputimage):
+    img = cv2.imread(inputimage)
+    def click_event(event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            print(x,', ',y)
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            strXY = str(x) + ', ' + str(y)
+            cv2.putText(img, strXY, (x,y), font, .4, (255,255,0), 2)
+            cv2.imshow('image', img)
+        if event == cv2.EVENT_RBUTTONDOWN:
+            blue = img[y,x,0]
+            green = img[y,x,1]
+            red = img[y,x,2]
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            strBGR = str(blue) + ', ' + str(green) + ', ' + str(red)
+            cv2.putText(img, strBGR, (x,y), font, .4, (0,255,255), 2)
+            cv2.imshow('image', img)
+        if event == cv2.EVENT_MOUSEMOVE:
+            blue = img[y,x,0]
+            green = img[y,x,1]
+            red = img[y,x,2]
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            strBGR = str(blue) + ', ' + str(green) + ', ' + str(red)
+            print(strBGR)
+    cv2.imshow('image', img)
+    cv2.setMouseCallback('image', click_event)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
