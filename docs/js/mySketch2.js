82f123fade64865291e0d2d53a95b9bb595b9e35
altunenes <enesaltun2@gmail.com>
2023-01-13 03:41:23 +0300

    a  major improvement for crazyEEG script

--- docs/js/mySketch2.js
+++ docs/js/mySketch2.js
@@ -1,35 +1,103 @@
-// create a canvas to draw on
+let xIncrementSlider, amplitudeSlider, frequencySlider, gridThicknessSlider, speed = 3;
+let speedUpButton, speedDownButton, bgColorPicker, gridColorPicker, sinColorPicker,cosColorPicker;
+let cosineToggle = false;
+let toggleButton;
+let sliderContainer;
+let buttonContainer;
+let colorContainer;
+let labelContainer;
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
+  sliderContainer = createDiv();
+  buttonContainer = createDiv();
+  colorContainer = createDiv();
+  labelContainer = createDiv();
+  xIncrementSlider = createSlider(1, 20, 4, 1);
+  amplitudeSlider = createSlider(50, 150, 90, 1);
+  frequencySlider = createSlider(1, 10, 1, 0.1);
+  gridThicknessSlider = createSlider(1, 20, 7, 1);
+  sliderContainer.child(xIncrementSlider);
+  sliderContainer.child(amplitudeSlider);
+  sliderContainer.child(frequencySlider);
+  sliderContainer.child(gridThicknessSlider);
+  sliderContainer.style("display: flex; flex-wrap:wrap; justify-content: space-between;");
+  speedUpButton = createButton("Speed -");
+  speedUpButton.mousePressed(increaseSpeed);
+  speedDownButton = createButton("Speed +");
+  speedDownButton.mousePressed(decreaseSpeed);
+  buttonContainer.child(speedUpButton);
+  buttonContainer.child(speedDownButton);
+  buttonContainer.style("display: flex; justify-content: space-between;");
+  bgColorPicker = createColorPicker("#FFFFFF");
+  gridColorPicker = createColorPicker("#000000");
+  sinColorPicker = createColorPicker("#FF0000");
+  cosColorPicker = createColorPicker("#0000FF");
+  colorContainer.child(bgColorPicker);
+  colorContainer.child(gridColorPicker);
+  colorContainer.child(sinColorPicker);
+  colorContainer.child(cosColorPicker);
+  colorContainer.style("display: flex; justify-content: space-between;");
+  toggleButton = createButton("Toggle Cosine Wave");
+  toggleButton.mousePressed(toggleCosineWave);
+  buttonContainer.child(toggleButton);
+  createLabel("X-Increment : ",xIncrementSlider);
+  createLabel("Amplitude : ",amplitudeSlider);
+  createLabel("Frequency : ",frequencySlider);
+  createLabel("Grid Thickness : ",gridThicknessSlider);
+  createLabel("Background Color : ",bgColorPicker);
+  createLabel("Grid Color : ",gridColorPicker);
+  createLabel("Sine Color : ",sinColorPicker);
+  createLabel("Cosine Color : ",cosColorPicker);
 }
-
-// define the sine wave pattern
-function drawSineWave(amplitude, frequency) {
-  stroke(255, 0, 0);
-  strokeWeight(2); // use the value of the lineThickness parameter to set the strokeWeight of the sine lines
-  noFill(0);
-
+function createLabel(label,element){
+  let labelElement = createDiv(label);
+  labelElement.child(element);
+  labelContainer.child(labelElement);
+}
+function drawSineWave() {
+  stroke(sinColorPicker.color());
+  strokeWeight(2);
+  noFill();
   beginShape();
-  for (let x = 0; x <= width; x += 2) {
-    let y = amplitude * sin(x / frequency) + height / 2;
+  for (let x = 0; x <= width; x += xIncrementSlider.value()) {
+    let y = amplitudeSlider.value() * sin(x / frequencySlider.value()) + height / 2;
     vertex(x, y);
   }
   endShape();
 }
-// define the moving grid
-function drawGrid(lineThickness, speed) {
-  stroke(0);
-  strokeWeight(lineThickness);
-  let t = frameCount / speed;
-  for (let x = 0; x < 150; x += 12) {
-    line(x + t, 0, x + t, height);
+function drawCosineWave() {
+  if (cosineToggle) {
+    stroke(cosColorPicker.color());
+    strokeWeight(2);
+    noFill();
+    beginShape();
+    for (let x = 0; x <= width; x += xIncrementSlider.value()) {
+      let y = amplitudeSlider.value() * cos(x / frequencySlider.value()) + height / 2;
+      vertex(x, y);
+    }
+    endShape();
   }
 }
-
-// draw the animation
-function draw() {
-  background(255, 255, 255); // set the background color to white
-  drawSineWave(90,1); // use a value of 100 for the amplitude and 10 for the frequency
-  drawGrid(9.5,3); // use a value of 7 for the line thickness and 2 for the speed
+function drawGrid() {
+  stroke(gridColorPicker.color());
+  strokeWeight(gridThicknessSlider.value());
+  let t = frameCount / speed;
+  for (let x = 0; x < 150; x += 12) {
+    line(x + t % width, 0, x + t % width, height);
+  }
 }
-
+function increaseSpeed() {
+  speed += 1;
+}
+function decreaseSpeed() {
+  speed -= 1;
+}
+function toggleCosineWave() {
+  cosineToggle = !cosineToggle;
+}
+function draw() {
+  background(bgColorPicker.color());
+  drawSineWave();
+  drawCosineWave();
+  drawGrid();
+}
